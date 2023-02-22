f14b90a94edbd8bb6ad4881ead2ef558e8ebf2cd
smrnvlz <khitrova08@gmail.com>
2023-02-22 23:09:06 +0300

    update

--- utils/arrs.py
+++ utils/arrs.py
@@ -11,10 +11,11 @@ def get(array, index, default=None):
     :param default: значение по-умолчанию.
     :return: значение по индексу или значение по-умолчанию.
     """
-    if index < 0:
+    if index not in array:
         return default
 
-    return array[index]
+    return array[-index]
+
 
 
 def my_slice(coll, start=0, end=None):
